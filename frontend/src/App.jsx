801dd3ff36486e7f20e6b15c6f0a684c8b84f84c
TheRermz <mufischer@hotmail.com>
2025-06-20 16:06:10 -0300

    Ajustes no main e app, adicionado novos pacotes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,21 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import Navbar from "../components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+// import Clientes from "./pages/Clientes";
+// import Pets from "./pages/Pets";
+// import Atendimentos from "./pages/Atendimentos";
 
 function App() {
-  const [clientes, setClientes] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Chamada da API
-    fetch("http://localhost:5029/api/Clientes")
-      .then((response) => {
-        if (!response.ok) throw new Error("Erro na requisição");
-        return response.json();
-      })
-      .then((data) => {
-        setClientes(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Erro ao buscar clientes:", error);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) return <p>Carregando...</p>;
-
   return (
-    <div>
-      <h2>Lista de Clientes</h2>
-      <ul>
-        {clientes.map((cliente) => (
-          <li key={cliente.id}>
-            {cliente.nome} - {cliente.telefone}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/clientes" element={<Clientes />} />
+        <Route path="/pets" element={<Pets />} />
+        <Route path="/atendimentos" element={<Atendimentos />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
